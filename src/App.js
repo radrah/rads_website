32214d1972393b5802d2928e041fd101dc44f014
radrah <87847961+radrah@users.noreply.github.com>
2023-10-29 17:44:31 +1300

    Building the Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>YO</h1>
-    </div>
+    <>
+    <Router>
+       <NavBar />
+       <Routes>
+        <Route path='/' exact />
+       </Routes>
+    </Router>
+     
+    </>
   );
 }
 
