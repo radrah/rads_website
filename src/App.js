a136800f38f4a32f0040621c992abc309f2a7e0c
radrah <87847961+radrah@users.noreply.github.com>
2023-10-30 21:02:24 +1300

    Added all pages and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Pages/Home.js'
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
 import './App.css';
 
 function App() {
@@ -9,7 +13,10 @@ function App() {
     <Router>
        <NavBar />
        <Routes>
-        <Route path='/' exact />
+        <Route path='/' exact Component={Home}/>
+        <Route path='/services' exact Component={Services}/>
+        <Route path='/products' exact Component={Products}/>
+        <Route path='/sign-up' exact Component={SignUp}/>
        </Routes>
     </Router>
      
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,7 @@ function NavBar() {
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to="/" className='navbar-logo'>
+            <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                 TRVL <i className='fab fa-typo3'/>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
