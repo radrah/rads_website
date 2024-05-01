db3d4679f289c3e229a2ee0e69e5d1f2ccb879ad
radrah <raduwan.rahman@hotmail.com>
2024-05-02 02:35:17 +1200

    Update readme and imagery

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
 	return (
 		<div className='hero-container'>
-			<video src = "/video{s/video-1.mp4" autoPlay loop muted />
+			<video src = "/videos/video-1.mp4" autoPlay loop muted />
 			<h1>Adventure Awaits<br></br>(WHERE YOU GIVE ME JOB)</h1>
 			<p>More updates coming soon so you give me a job</p>
 			<div className='hero-btns'>
